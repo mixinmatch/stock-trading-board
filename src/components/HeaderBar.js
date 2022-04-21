8deb528bbfb96a98b1c307f770f6663e728b1d55
Wayne <chevreuilqwerty@gmail.com>
2022-04-21 18:16:40 -0400

    add routing

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,5 +1,8 @@
 import Button from "./Button"
-
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 const HeaderBar = () => {
     return (
         <div>
@@ -20,11 +23,21 @@ const SearchBar = () => {
 }
 
 const RightMenu = () => {
+    const textStyle = {textDecoration: 'none', fontWeight:"bold", color: "black" }
     return (
          <div className="right-button-group">
-         <Button text="Home" className="right-menu-button" textfmt="button-fmt"/> 
-         <Button text="Notifications" className="right-menu-button" textfmt="button-fmt"/> 
-         <Button text="Account" className="right-menu-button" textfmt="button-fmt"/>
+            <Router>
+                <button className="right-menu-button">
+                    <Link to="home" style={textStyle} >Home</Link>
+                </button>
+                <button className="right-menu-button">
+                    <Link to="notifications" style={textStyle} >Notifications</Link>
+                </button>
+                <button className="right-menu-button">
+                    <Link to="account" style={textStyle} >Account</Link>
+                </button>
+
+            </Router>
          </div>
     )
 }
