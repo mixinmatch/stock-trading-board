eb940732ffe5cea99337cf98336c5ab0edfe1363
W <chevreuilqwerty@gmail.com>
2022-04-28 21:13:22 -0400

    fix overflow text

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -7,7 +7,9 @@ const NewsCard = ({imgSrc}) => {
                 <span className="news-header">
                 Lorem ipsum dolor sit amet
                 </span>
+            <span className="news-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie nibh vel lectus ullamcorper, et rutrum dolor egestas. Pellentesque quis erat a tellus suscipit fermentum. Donec risus leo, tristique eget elit nec, luctus consequat nisl. Mauris enim libero, cursus a dui quis, porta lobortis elit. Cras sit amet lorem in justo maximus interdum.
+            </span>
             </span>
         </div>
     )
