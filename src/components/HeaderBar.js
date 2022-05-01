fddc574eb882a1404ccadc5f85bca1100c757730
W <chevreuilqwerty@gmail.com>
2022-05-01 18:12:02 -0400

    fix logo

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -22,7 +22,7 @@ const HeaderBar = ({handler, renderSearch, setRenderSearch}) => {
         <div>
             <div className="header-bar">
                 <div className="logo-and-search">
-                <img src={"img/logo2.svg"} style={{padding:'20'}}/>
+                <img src={"img/logo2.svg"} style={{padding:'12px'}}/>
                 <input className="header-bar-search" type="text" placeholder="Search a stock" onInput={onSearchChange} />
                 </div>
                 <RightMenu />
